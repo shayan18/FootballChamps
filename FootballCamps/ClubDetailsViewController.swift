192978b63adbef3b54673c49702eedddde194933
Shayan Ali <shayan.ali@veripark.com>
2019-10-12 15:03:11 +0500

    project setup

---
+++ FootballCamps/ClubDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClubDetailsViewController.swift
+//  FootballCamps
+//
+//  Created by Shayan Ali on 12/10/2019.
+//  Copyright © 2019 Shayan Ali. All rights reserved.
+//
+
+import UIKit
+
+class ClubDetailsViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
