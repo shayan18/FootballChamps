196c8bb29c0460bfe719a046cf8f66735e7a0888
Shayan Ali <shayan.ali@veripark.com>
2019-10-12 15:30:05 +0500

    design structured

---
+++ FootballCamps/Views/ClubTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ClubTableViewCell.swift
+//  FootballCamps
+//
+//  Created by Shayan Ali on 12/10/2019.
+//  Copyright © 2019 Shayan Ali. All rights reserved.
+//
+
+import UIKit
+
+class ClubTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak private var logoImageView: UIImageView!
+    @IBOutlet weak private var nameLabel: UILabel!
+    @IBOutlet weak private var countryLabel: UILabel!
+        @IBOutlet weak private var worthLabel: UILabel!
+    
+}
